3a17563bc4aaf5ff45ee39617396e2b2b981c1cb
geeteshkumbalkar028 <geeteshkumbalkar@gmail.com>
2023-04-08 13:29:15 +0530

    Added : Ability for the Greeting App to give Greeting message

--- src/main/java/com/bridgelabz/GreetingApp/services/GreetingImp.java
+++ src/main/java/com/bridgelabz/GreetingApp/services/GreetingImp.java
@@ -1,7 +1,25 @@
 package com.bridgelabz.GreetingApp.services;
 
+import com.bridgelabz.GreetingApp.dto.GreetingDTO;
+
 public class GreetingImp {
     public String message(){
         return "Hello world";
+    }
+    public String message(GreetingDTO greetingDTO){
+        if((greetingDTO.firstName)==null && (greetingDTO.lastName)==null ){
+            return "Hello World";
+        }
+        else if((greetingDTO.firstName)==null){
+            return greetingDTO.lastName;
+        }
+        else if((greetingDTO.lastName)==null){
+            return greetingDTO.firstName;
+        }
+        else
+        {
+            return greetingDTO.toString();
+        }
+
     }
 }
